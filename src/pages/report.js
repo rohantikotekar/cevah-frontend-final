46f4e76b26543beca33e9cea7bb0fa75c3331e52
sohan-412 <sohanwagh.10@gmail.com>
2024-05-25 15:43:19 +0530

    Sohan

--- src/pages/report.js
+++ src/pages/report.js
@@ -5,6 +5,7 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 import GroupComponent from '../components/GroupComponent';
 
+
 const Report = () => {
   const navigate = useNavigate();
 
